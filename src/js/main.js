d4af64b8c7229c5a521e57d688a10364f82b3979
Florian Bar <florianbar01@gmail.com>
2017-06-21 12:09:41 +0200

    removed favicon

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
+function printRepoCount() {
+ 	var responseObj = JSON.parse(this.responseText);
+	//console.log(responseObj.name + " has " + responseObj.public_repos + " public repositories!");
+	console.log(responseObj);
+}
 
+var request = new XMLHttpRequest();
+request.onload = printRepoCount;
+request.open('get', 'https://api.github.com/users/funchal', true);
+request.send();
